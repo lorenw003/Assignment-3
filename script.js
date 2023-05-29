231baf6c66d3865146909037bb68e1a1a842f57e
CGS-Loren-Wilson <37096710+CGS-Loren-Wilson@users.noreply.github.com>
2023-05-29 19:08:08 +1000

    product page

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// const onClick = function() {
+//   console.log(this.id, this.innerHTML);
+//   if (this.id == "info") {
+//     document.getElementById("details").innerText = "Tanzanite, an Italian-made vintage combo organ with a rare earthy color. VOX Jaguar-like combo organ specifications for bass and tone control and sound. We have customized the Italian power supply AC220V specification to an AC adapter, so you can use it as it is with a Japanese specification 100V power outlet.";
+//   } else if (this.id == "condition") {
+//     document.getElementById("details").innerText = "Maintenance completed and fully working (all electrolytic capacitors replaced, semi-fixed resistors all replaced, BASS key contact cleaning & position adjustment, 440Hz tuning, volume pot cleaning [no gully due to operation], internal cleaning, etc.) / AC adapter jack (2.1 mm) Newly installed / Replaced power light with LED / Minor scratches and blurring overall (2020.9.28)";
+//   } else {
+//     document.getElementById("details").innerText = "If a defect other than the condition notation on the product page occurs during normal use within one year after purchase, we will repair it free of charge or irreparable. In that case, we will accept returns. For the first warranty repair within the warranty period, we will bear the shipping fee, but for the second and subsequent repairs within the warranty period, please only pay the shipping fee (shipping & return). For products that have passed one year after purchase, we will repair at the actual cost (labor, parts, shipping). If you let us know about the problem in advance, we will give you a rough estimate of the repair cost. However, please note that there may be cases where repair is not possible.";
+//   }
+// }
+
+// document.getElementById('info').onclick = onClick;
+// document.getElementById('care').onclick = onClick;
+// document.getElementById('condition').onclick = onClick;
+
+
 function getCart () {
   var modal = document.getElementById("cartModal");
 
